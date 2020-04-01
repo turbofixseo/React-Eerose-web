d43395be4ab20da393303217402817f5026bd978
turbofixseo <turbofixseo@gmail.com>
2020-03-31 23:12:19 -0700

    build home page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import './App.css';
+import GoogleFontLoader from 'react-google-font-loader';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <div className='header-bar'>
+        <div className='logo'>
+          <GoogleFontLoader
+            fonts={[
+              {
+                font: 'Permanent Marker',
+                weights: [400, 700],
+              },
+              {
+                font: 'Syncopate',
+                weights: [400, 700],
+              },
+              {
+                font: 'Rasa',
+                weights: [400, 700],
+              }
+            ]}
+            subsets={['cyrillic-ext', 'greek']}
+          />
+          <div className='logo-contanier'>
+            <p className='logo-ar'>AR</p>
+            <p className='logo-large'>HERoEs</p>
+          </div>
+        </div>
+      </div>
+      <div className='content'>
+        <div className='preview-text'>
+          <p>AR World</p>
+          <p>3D dragon hero fighting</p>
+          <p>beathing fire</p>
+        </div>
+      </div>
+      <div className='footer-bar'>
+        
+      </div>
     </div>
   );
 }
